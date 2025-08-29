730f5b91db7919e9799fa7abdc408cd9d4fbbd54
SamFlorence sio <samflorence2006@gmail.com>
2025-08-29 22:02:22 +0800

    Activity10

---
+++ Activity10.py
@@ -0,0 +1,11 @@
+username = "Sam"
+password = "Pass"
+
+user = input("Enter username -->")
+pwss = input("Enter password -->")
+
+if (user == username) and (pwss == password):
+	print("Access Granted")
+
+else:
+	print("Access Denied")
