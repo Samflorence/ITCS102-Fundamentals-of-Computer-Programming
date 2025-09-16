caf07638d7eb951439e64b993bde9793127195b4
Samflorence <73792237+Samflorence@users.noreply.github.com>
2025-09-16 09:13:59 +0800

    Add files via upload

---
+++ Code_Challenge5.py
@@ -0,0 +1,8 @@
+print("THE FACTORIAL PROGRAM")
+number = eval(input("Enter any number ---> "))
+
+factorial = 1
+for x in range(number,0, -1):
+    factorial *= x
+    
+print('The factorial of ',number,'is',factorial)
