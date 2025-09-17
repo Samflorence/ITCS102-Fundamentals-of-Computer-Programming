c4cced914fd3df00d91cafa76887f082b047e56d
Samflorence <73792237+Samflorence@users.noreply.github.com>
2025-09-17 15:34:58 +0800

    Add files via upload

---
+++ Code_Challenge7.py
@@ -0,0 +1,9 @@
+#Countdown Timer
+
+down = eval(input('Enter the starting number for countdown:'))
+
+print('\nCountdown Started')
+for x in range(down, 0, -1):
+    print(x)
+    
+print('liftoff!')    
