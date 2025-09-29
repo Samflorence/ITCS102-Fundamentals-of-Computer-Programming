f4cca1c2dc80b432d506b89b018417c07d70f128
Samflorence <73792237+Samflorence@users.noreply.github.com>
2025-09-29 22:11:22 +0800

    Add files via upload

---
+++ Activity20.py
@@ -0,0 +1,17 @@
+for x in range(1, 11,1):
+    for i in range(10, x, -1):
+        print(" ", end=" ")
+    for b in range(1, x, 1):
+        print('*',end=" ")
+    for v in range(1, x, 1):
+        print("*",end=" ")
+    print()
+
+for x in range(1, 11,1):
+    for i in range(1, x, 1):
+        print(" ", end=" ")
+    for b in range(10, x, -1):
+        print('*',end=" ")
+    for v in range(10, x, -1):
+        print("*",end=" ")
+    print()
