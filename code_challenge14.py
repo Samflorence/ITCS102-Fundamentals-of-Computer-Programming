2f6ae55eaba31e95b48f7060383da50001cd0314
Samflorence <73792237+Samflorence@users.noreply.github.com>
2025-11-19 15:53:23 +0800

    Add files via upload

--- code_challenge14.py
+++ code_challenge14.py
@@ -1,4 +1,5 @@
 import os 
+import json
 os.system('cls')
 print("Student Information System")
 print("=============================")
@@ -12,7 +13,7 @@ while True:
         print("\nAdding new student record")
 
         id_no = input("Please Input Student ID number:")
-        first_name = input("PLease input first name:").upper()
+        first_name = input("Please input first name:").upper()
         last_name = input('Please input last name:').upper()
         age = eval(input('Please input age:'))
         section = input('Please input section:').upper()
@@ -23,19 +24,79 @@ while True:
     elif choice == 'b':
         os.system('cls')
         print('Printing Student Record')
-        print(student_record)
+        
+        for x, i in student_record.items():#key - values 
+            print(f'student - {x}, Information - {i}')
         continue
     elif choice == 'c':
-        pass
+        os.system('cls')
+        print('Search Student Record')
+        search_id = input('Input Student Id Number to Search: ').upper()
+        for each_id in student_record.keys():
+            if search_id in student_record.keys():
+                print('++++++++++++++++++++++++++++++++')
+                print(f'Record Found for ID {search_id}')
+                for x in student_record[search_id]:
+                    print(f'--- {x}')
+                print('++++++++++++++++++++++++++++++++')
+            else:
+                print('Record Not Found')
+            break   
         continue
     elif choice == 'd':
-        pass
+        os.system('cls')
+        print('Delete Student Record')
+        delete_id = input('Input Student Id Number to be Delete: ').upper()
+        for each_id in student_record.keys():
+            if delete_id in student_record.keys():
+                print('++++++++++++++++++++++++')
+                print(f'Record Found for ID {delete_id}')
+                for x in student_record[delete_id]:
+                    print(f'--- {x}')
+                print('++++++++++++++++++++++++')
+
+                student_record.pop(delete_id)
+                print('\nRecord Deleted')
+            else:
+                print('Record Not Found')
+            break   
         continue
     elif choice == 'e':
-        pass
+        print('Edit Student Record')
+        edit_it = input('Input Student Id Number to Edit: ').upper()
+        for each_id in student_record.keys():
+            if edit_it in student_record.keys():
+                print('++++++++++++++++++++++++++++++++')
+                print(f'Record Found for ID {edit_it}')
+                for x in student_record[edit_it]:
+                    print(f'--- {x}')
+                print('++++++++++++++++++++++++++++++++')
+                
+                first_name = input("Please input first name:").upper()
+                last_name = input('Please input last name:').upper()
+                age = eval(input('Please input age:'))
+                section = input('Please input section:').upper()
+                course = input('Please input course:').upper()
+
+                student_record[edit_it][0] = first_name
+                student_record[edit_it][1] = last_name
+                student_record[edit_it][2] = age
+                student_record[edit_it][3] = section
+                student_record[edit_it][4] = course
+
+                print('\nRecord Edited')
+            else:
+                print('Record Not Found')
+            break   
         continue
     elif choice == 'f':
-        pass
+        os.system('cls')
+        print('Export Student Data')
+        #json Javascript Object Notation
+        with open ('student_record','w') as new_file:
+            json.dump(student_record,new_file, indent=4)
+        
+        print('\nExport Sucessfull')
         continue
     elif choice == 'g':
         print("Exit System")
