56423915ad31f81a7ad8f949c206c21cdd214de4
Samflorence <73792237+Samflorence@users.noreply.github.com>
2025-12-09 14:11:11 +0800

    Learning

---
+++ Project/Learnings.py
@@ -0,0 +1,13 @@
+def printing():
+   print('===================================')
+   print("\nprint('Hello World')")
+   print('\nExplanation')
+   print('\n=================================')
+   input('Press Enter to continue: ')
+
+def inputing():
+   print("input('Enter Any word')")
+   print('===================================')
+
+
+   print('\n=================================')
