5e17c342100cca01eb6252ed72742e50ee33049b
Samflorence <73792237+Samflorence@users.noreply.github.com>
2025-12-09 16:24:11 +0800

    ayusin mo yung try it your self

--- Project/Main.py
+++ Project/Main.py
@@ -1,28 +1,29 @@
 import os
 from Learning import *
+import code
 print('Learn Python In Weeks Using This Code')
 while True:
     print('\n===========================================')
     print('\n\t\t1 - 1st Week')
-    print('\n\t\t2 - 2nd Week')
-    print('\n\t\t3 - 3rd Week')
-    print('\n\t\t4 - 4th Week')
-    print('\n\t\t5 - Challenges')
-    print('\n\t\tX - Exit')
+    print('\t\t2 - 2nd Week')
+    print('\t\t3 - 3rd Week')
+    print('\t\t4 - 4th Week')
+    print('\t\t5 - Challenges')
+    print('\t\tX - Exit')
     print('\n===========================================')
     count = input('\nPlease Select From Above: ').lower()
-    
+    os.system('cls')
     if count == '1':
         os.system('cls')
         while True:
             print('\n~~~~~~~~~~~~~~~First Week~~~~~~~~~~~~~~~~~')
             print('\n\tA - What Is Python')
-            print('\n\tB - Using the print() Function')
-            print('\n\tC - Using the input() Function')
-            print('\n\tD - Using the _______ Function')
-            print('\n\tS - Try it yourself')
-            print('\n\tT - Activity')
-            print('\n\tX - Go Back')
+            print('\tB - Using the print() Function')
+            print('\tC - Using the input() Function')
+            print('\tD - Using the _______ Function')
+            print('\tS - Try it yourself')
+            print('\tT - Activity')
+            print('\tX - Go Back')
             print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
             day = input('Please Select From Above: ').lower()
             
@@ -30,25 +31,27 @@ while True:
                 os.system('cls')
                 print('Coding is a coding language that is beginer friendly')
                 input('Press Enter to continue: ')
-                continue
             elif day == 'b':
                 os.system('cls')
                 printing()
-                continue
             elif day == 'c':
                 os.system('cls')
-                pass
-                continue
+                inputing()
             elif day == 'd':
                 os.system('cls')
-                pass
-                continue
+                
+            elif day == 's':
+                os.system('cls')
+                console = code.InteractiveConsole(globals())
+                console.push("print('Welcome to the interactive console!')")
+                print('To exit input exit()')
+                console.interact("Enter your code below: ")
             elif day == 'x':
+                print('Going Back')
                 break
             else:
                 os.system('cls')
-                print('Invalid Input Please Choose Again')
-                continue    
+                print('Invalid Input Please Choose Again')   
         continue
     elif count == '2':
         pass
