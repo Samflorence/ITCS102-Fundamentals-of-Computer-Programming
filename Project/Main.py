1b1982a4d502667b3acf6fbda4c8a1e2dac4f0e0
Samflorence <73792237+Samflorence@users.noreply.github.com>
2025-12-10 14:41:56 +0700

    Update Main.py

--- Project/Main.py
+++ Project/Main.py
@@ -1,5 +1,5 @@
 import os
-from Learning import *
+from learning import *
 import code
 print('Learn Python In Weeks Using This Code')
 while True:
@@ -14,47 +14,75 @@ while True:
     count = input('\nPlease Select From Above: ').lower()
     os.system('cls')
     if count == '1':
-        os.system('cls')
         while True:
             print('\n~~~~~~~~~~~~~~~First Week~~~~~~~~~~~~~~~~~')
             print('\n\tA - What Is Python')
             print('\tB - Using the print() Function')
             print('\tC - Using the input() Function')
-            print('\tD - Using the _______ Function')
+            print('\tD - Using the ___ Function')
             print('\tS - Try it yourself')
             print('\tT - Activity')
             print('\tX - Go Back')
             print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
             day = input('Please Select From Above: ').lower()
-            
+            os.system('cls')
             if day == 'a':
                 os.system('cls')
                 print('Coding is a coding language that is beginer friendly')
                 input('Press Enter to continue: ')
+                continue
             elif day == 'b':
                 os.system('cls')
                 printing()
+                continue
             elif day == 'c':
                 os.system('cls')
                 inputing()
+                continue
             elif day == 'd':
                 os.system('cls')
-                
+                continue
             elif day == 's':
-                os.system('cls')
-                console = code.InteractiveConsole(globals())
-                console.push("print('Welcome to the interactive console!')")
-                print('To exit input exit()')
-                console.interact("Enter your code below: ")
+                # os.system('cls')
+                # console = code.InteractiveConsole(globals())    
+                # console.push("print('Welcome to the interactive console!')")
+                # print('To exit input exit()')
+                # console.interact("Enter your code below: ")
+                continue
             elif day == 'x':
                 print('Going Back')
                 break
             else:
                 os.system('cls')
-                print('Invalid Input Please Choose Again')   
+                print('Invalid Input Please Choose Again')
+                continue
         continue
     elif count == '2':
-        pass
+        while True:
+            print('\n~~~~~~~~~~~~~~~Second Week~~~~~~~~~~~~~~~~~')
+            print('\n\tA - ')
+            print('\n\tB - ')
+            print('\n\tC - ')
+            print('\n\tD - ')
+            print('\n\tE - ')
+            print('\n\tF - ')
+            print('\n\tG - ')
+            print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            day = input('Please Select From Above: ').lower()
+            if day == 'a':
+                pass
+                continue
+            elif day == 'b':
+                pass 
+                continue
+            elif day == 'c':
+                pass
+                continue
+            elif day == 'x':
+                break
+            else:
+                os.system('cls')
+                print('Invalid Input Please Choose Again')
         continue
     elif count == '3':
         pass
