f3b166b2945eb62969f4d66344e0f9f633c63098
Samflorence <73792237+Samflorence@users.noreply.github.com>
2025-12-11 15:46:35 +0800

    Update Learnings.py

--- Project/Learnings.py
+++ Project/Learnings.py
@@ -1,13 +1,55 @@
+#print
 def printing():
    print('===================================')
    print("\nprint('Hello World')")
    print('\nExplanation')
    print('\n=================================')
    input('Press Enter to continue: ')
-
+#input
 def inputing():
    print("input('Enter Any word')")
    print('===================================')
    print("input(')")
 
    print('\n=================================')
+#escape sequence
+
+#len
+
+#operatiors
+
+#assignment operators
+
+#rational operators 
+
+#logical operators
+
+#conditional statement 
+
+#if and elif 
+
+#loop
+
+#sum automation 
+
+#descending loop 
+
+#string formating 
+
+#horizontal end (descending)
+
+#nested loop (descending)
+
+#diamond using loop
+
+#while loop 
+
+#import ramdom 
+
+#list operation 
+
+#importing def
+
+#dictional operation
+
+#snake game
